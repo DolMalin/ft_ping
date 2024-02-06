2287cb800cb654086f310215342119e3363eb519
DolMalin <dolmalinn@gmail.com>
2024-02-06 16:55:33 +0100

    feat: send icmp packet to dest

--- includes/main.h
+++ includes/main.h
@@ -1,8 +1,12 @@
 #pragma once
 
 # include <sys/socket.h>
-# include <ip.h>
+# include <linux/icmp.h>
+# include <linux/ip.h>
 # include <netinet/in.h>
+# include <arpa/inet.h>
+# include <stdio.h>
 # include "../libft/libft.h"
 
 #define PACKET_BUFFER_SIZE 4096
+#define ID 59321
--- srcs/main.c
+++ srcs/main.c
@@ -1,27 +1,62 @@
 #include "../includes/main.h"
 
 
+struct packet {
+    struct iphdr ip_header;
+    struct icmphdr icmp_header;
+    char msg[256]; // todo
+};
+
+
 int main(int ac, char **av)
 {
+    (void)ac;
+    (void)av;
+
     // Creating raw socket
-    int socket_fd = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
-    if (socket_fd == -1)
+    int socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (socket_fd == -1) {
+        printf("cannot create socket");
         return 1;
+    }
+
+    struct packet packet;
+    ft_memset(&packet, 0, sizeof(packet));
+
+    // Setup addr
+    struct sockaddr_in source_addr, dest_addr;
+    source_addr.sin_family = AF_INET;
+    dest_addr.sin_family = AF_INET;
+
+    inet_pton(AF_INET, av[1], &source_addr.sin_addr.s_addr);
+    inet_pton(AF_INET, "127.0.0.1", &dest_addr.sin_addr.s_addr);
 
     //  Setup ip header
-    char packet_buffer[PACKET_BUFFER_SIZE];
-    ft_bzero(packet_buffer, PACKET_BUFFER_SIZE);
-    struct iphdr *ip_header = (struct iphdr *)packet_buffer;
+    packet.ip_header.ihl = 5;
+    packet.ip_header.version = 4;
+    packet.ip_header.tos = 0;
+    packet.ip_header.id = htons(ID);
+    packet.ip_header.frag_off = 0;
+    packet.ip_header.ttl = 255;
+    packet.ip_header.protocol = 0;
+    packet.ip_header.check = 0;
+    packet.ip_header.tot_len = htons(sizeof(packet));
+    packet.ip_header.saddr = source_addr.sin_addr.s_addr;
+    packet.ip_header.daddr = dest_addr.sin_addr.s_addr;
 
-    ip_header->ihl = 5;
-    ip_header->version = 4;
-    ip_header->tos = 0;
-    // iph->tot_len // to setup
-    ip_header->id = htons(59231);
-    ip_header->frag_off = 0;
-    ip_header->ttl = 255;
-    ip_header->protocol = IPPROTO_TCP;
-    ip_header->check = 0;
-    ip_header->saddr = inet_addr()
+    // Setup icmp header
+    packet.icmp_header.type = ICMP_ECHO;
+    packet.icmp_header.code = 0;
+    packet.icmp_header.checksum = 0;
+    packet.icmp_header.un.echo.id = 12345;
+    packet.icmp_header.un.echo.sequence = 0;
+
+
+    packet.msg[0] = 'C';
+
+    int ret = sendto(socket_fd, &packet, sizeof(struct packet), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (ret == -1)
+        printf("err send\n");
+
 
 }
